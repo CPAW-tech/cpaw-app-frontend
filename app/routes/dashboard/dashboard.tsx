864bc1259426084ed7c03356c2b930b8ba9fad3a
regexbuster <benjamin@gofranks.com>
2025-07-31 16:13:02 -0400

    dashboard test user state setup

--- app/routes/dashboard/dashboard.tsx
+++ app/routes/dashboard/dashboard.tsx
@@ -1,21 +1,40 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Dashboard() {
+    const [userDetails, setUserDetails] = useState({ username: '' })
+
     useEffect(() => {
         const testProtected = async () => {
-            await fetch('http://localhost:3000/api/dashboard', {
-                method: 'GET',
-                headers: { 'Content-Type': 'application/json' },
-                credentials: 'include', //allows cookies to be set
-            }).catch((e) => {
-                console.error(e)
-            })
+            let { ok, data, metadata }: any = await fetch(
+                'http://localhost:3000/api/dashboard',
+                {
+                    method: 'GET',
+                    headers: { 'Content-Type': 'application/json' },
+                    credentials: 'include', //allows cookies to be set
+                }
+            )
+                .then(async (res: Response) => {
+                    return await res.json()
+                })
+                .catch((e) => {
+                    console.error(e)
+                })
+
+            console.log('useEffect', ok, data, metadata)
+
+            if (!ok) {
+                console.error(data)
+            }
+
+            setUserDetails(data)
         }
         testProtected()
     }, [])
+
     return (
         <>
             <h1>Dashboard!</h1>
+            <p>{`Hello ${userDetails.username}`}</p>
             {/* <p>{JSON.stringify(userAuth)}</p> */}
         </>
     )
